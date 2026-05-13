8ae1c6089ef904ffe3ac2289170cad19a268de91
peterdklee <wehanada1@gmail.com>
2026-05-13 21:26:58 +0900

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,14 @@
+import './globals.css'
 
+export const metadata = {
+  title: 'Olymptrade X Fintrix',
+  description: 'Mock Trading Competition',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  )
+}
